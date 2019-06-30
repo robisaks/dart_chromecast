5723e47de619f6ba5f0b6da1ca0390c7ceed1aed
Rob <rob.isakson@gmail.com>
2019-06-30 14:31:53 -0500

    Update text_channel.dart

--- lib/casting/text_channel.dart
+++ lib/casting/text_channel.dart
@@ -4,7 +4,7 @@ import 'package:dart_chromecast/casting/cast_channel.dart';
 
 class TextChannel extends CastChannel {
 
-  MediaChannel.Create({Socket socket, String sourceId, String destinationId}) :
+  TextChannel.Create({Socket socket, String sourceId, String destinationId}) :
       super.CreateWithSocket(socket, sourceId: sourceId, destinationId: destinationId, namespace: 'urn:x-cast:textChannel');
 
 }
