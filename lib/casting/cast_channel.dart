74ef728569683f1c0faee93e8f98c3b99fa339a1
Rob <rob.isakson@gmail.com>
2019-06-30 14:24:49 -0500

    Update cast_channel.dart

--- lib/casting/cast_channel.dart
+++ lib/casting/cast_channel.dart
@@ -22,6 +22,39 @@ abstract class CastChannel {
       _destinationId = destinationId,
       _namespace = namespace;
   
+  void sendTextMessage(Map payload) async {
+
+    payload['requestId'] = _requestId;
+
+    CastMessage castMessage = CastMessage();
+    castMessage.protocolVersion = CastMessage_ProtocolVersion.CASTV2_1_0;
+    castMessage.sourceId = _sourceId;
+    castMessage.destinationId = _destinationId;
+    castMessage.namespace = _namespace;
+    castMessage.payloadType = CastMessage_PayloadType.JSON;
+    castMessage.payloadUtf8 = jsonEncode(payload);
+
+    Uint8List bytes = castMessage.writeToBuffer();
+    Uint32List headers = Uint32List.fromList(writeUInt32BE(List<int>(4), bytes.lengthInBytes));
+    Uint32List fullData = Uint32List.fromList(headers.toList()..addAll(bytes.toList()));
+
+    if ('PING' != payload['type']) {
+
+      print('Send: ${castMessage.toDebugString()}');
+      print('List: ${fullData.toList().toString()}');
+
+    }
+    else {
+
+      print('PING');
+
+    }
+
+    _socket.add(fullData);
+
+    _requestId++;
+
+  }
   void sendMessage(Map payload) async {
 
     payload['requestId'] = _requestId;
